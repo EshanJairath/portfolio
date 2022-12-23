b26738b5cdc8e977636bbe696a301572ced850e6
Eshan Jairath <eshanjairath@gmail.com>
2022-12-23 13:29:52 +0000

    home spelling corrected

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Eshan Jaiath </strong>
+                <strong className="main-name"> Eshan Jairath </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
